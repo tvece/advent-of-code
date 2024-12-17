5a0008053bc53c33e91a379c3eb47db677b1d2f6
tvece <t.vecera97@gmail.com>
2024-12-17 17:50:49 +0100

    D16 part 1 (2024)

---
+++ src/main/java/com/advent/_2024/D16.java
@@ -0,0 +1,102 @@
+package com.advent._2024;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+//TODO part 2
+public class D16 {
+    public static void main(String[] args) {
+        Path filePath = Paths.get("src/main/resources/2024/D16.txt");
+        List<String> map;
+        try {
+            map = Files.readAllLines(filePath, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read input data!", e);
+        }
+        int startRow = 0;
+        int startColumn = 0;
+        startSearch:
+        for (int i = 0; i < map.size(); i++) {
+            for (int j = 0; j < map.getFirst().length(); j++) {
+                if (map.get(i).charAt(j) == 'S') {
+                    startRow = i;
+                    startColumn = j;
+                    break startSearch;
+                }
+            }
+        }
+        System.out.println(getShortestPath(map, startRow, startColumn));
+    }
+
+    private static int getShortestPath(List<String> map, int startRow, int startColumn) {
+
+        int[][][] distances = new int[map.size()][map.getFirst().length()][4];
+        for (int[][] directionCost : distances) {
+            for (int[] row : directionCost) {
+                Arrays.fill(row, Integer.MAX_VALUE);
+            }
+        }
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(startRow, startColumn, 0, 3));
+        distances[startRow][startColumn][3] = 0;
+
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+
+        while (!pq.isEmpty()) {
+            Node currentNode = pq.poll();
+            if (map.get(currentNode.row).charAt(currentNode.column) == 'E') {
+                return currentNode.distance;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nextRow = currentNode.row + dr[i];
+                int nextColumn = currentNode.column + dc[i];
+
+                if (map.get(nextRow).charAt(nextColumn) == '#') continue; // Skip walls
+
+                int turnCost = (i == currentNode.direction) ? 1 : 1001; // Cost for turning or keeping direction
+                int newDistance = currentNode.distance + turnCost;
+
+                if (newDistance < distances[nextRow][nextColumn][i]) {
+                    distances[nextRow][nextColumn][i] = newDistance;
+                    pq.add(new Node(nextRow, nextColumn, newDistance, i));
+                }
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    private static int[][] getNeighbors(int rowDirection, int columnDirection) {
+        if (rowDirection != 0) {
+            return new int[][]{new int[]{0, 1}, new int[]{0, -1}};
+        }
+        return new int[][]{new int[]{1, 0}, new int[]{-1, 0}};
+    }
+
+    private static class Node implements Comparable<Node> {
+        int row;
+        int column;
+        int distance;
+        int direction;
+
+        Node(int row, int column, int distance, int direction) {
+            this.row = row;
+            this.column = column;
+            this.distance = distance;
+            this.direction = direction;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.distance, other.distance);
+        }
+    }
+}
