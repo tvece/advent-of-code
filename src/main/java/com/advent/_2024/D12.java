5214c1c581f6ae7e24369e596b5af49b2c3163da
tvece <t.vecera97@gmail.com>
2024-12-19 20:04:50 +0100

    D12 part 2 (2024)

--- src/main/java/com/advent/_2024/D12.java
+++ src/main/java/com/advent/_2024/D12.java
@@ -5,10 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class D12 {
     public static void main(String[] args) {
@@ -35,7 +32,7 @@ public class D12 {
                                 currentElement[1] >= 0 && currentElement[1] < map.getFirst().length() && // in range
                                 map.get(currentElement[0]).charAt(currentElement[1]) == group.character) {
                             if (!processed[currentElement[0]][currentElement[1]]) {
-                                group.addMember();
+                                group.addMember(currentElement[0], currentElement[1]);
                                 processed[currentElement[0]][currentElement[1]] = true;
                                 queue.add(new int[]{currentElement[0] + 1, currentElement[1]});
                                 queue.add(new int[]{currentElement[0] - 1, currentElement[1]});
@@ -54,13 +51,19 @@ public class D12 {
         for (Group group : groups) {
             result += (long) group.area * group.borders;
         }
-        System.out.println(result);
+        System.out.println("Part 1: " + result);
+        long result2 = 0;
+        for (Group group : groups) {
+            result2 += (long) group.area * group.getSides();
+        }
+        System.out.println("Part 2: " + result2);
     }
 
     private static class Group {
         final char character;
         int borders;
-        int area;
+        List<Point> points = new ArrayList<>();
+        private int area;
 
         Group(char character) {
             this.character = character;
@@ -68,12 +71,126 @@ public class D12 {
             this.area = 0;
         }
 
-        void addMember() {
+        void addMember(int row, int column) {
             this.area++;
+            points.add(new Point(row, column));
         }
 
         void addBorder() {
             this.borders++;
+        }
+
+        int getSides() {
+            int sides = 0;
+            Map<Integer, List<Point>> rows = new HashMap<>();
+            Map<Integer, List<Point>> columns = new HashMap<>();
+            this.points.forEach(point -> {
+                if (rows.containsKey(point.row)) {
+                    rows.get(point.row).add(point);
+                } else {
+                    rows.put(point.row, new ArrayList<>(Arrays.asList(new Point[]{point})));
+                }
+                if (columns.containsKey(point.column)) {
+                    columns.get(point.column).add(point);
+                } else {
+                    columns.put(point.column, new ArrayList<>(Arrays.asList(new Point[]{point})));
+                }
+            });
+            rows.forEach((id, row) -> Collections.sort(row));
+            columns.forEach((id, column) -> Collections.sort(column));
+
+            //top
+            for (Integer key : rows.keySet()) {
+                List<Point> row = rows.get(key);
+                Set<Point> sideIndicators = new HashSet<>();
+                for (Point point : row) {
+                    if (!points.contains(new Point(point.row - 1, point.column))) {
+                        if (!sideIndicators.contains(new Point(point.row, point.column - 1))) {
+                            sides++;
+                        }
+                        sideIndicators.add(point);
+                    }
+                }
+            }
+
+            //bottom
+            for (Integer key : rows.keySet()) {
+                List<Point> row = rows.get(key);
+                Set<Point> sideIndicators = new HashSet<>();
+                for (Point point : row) {
+                    if (!points.contains(new Point(point.row + 1, point.column))) {
+                        if (!sideIndicators.contains(new Point(point.row, point.column - 1))) {
+                            sides++;
+                        }
+                        sideIndicators.add(point);
+                    }
+                }
+            }
+
+            //left
+            for (Integer key : columns.keySet()) {
+                List<Point> column = columns.get(key);
+                Set<Point> sideIndicators = new HashSet<>();
+                for (Point point : column) {
+                    if (!points.contains(new Point(point.row, point.column - 1))) {
+                        if (!sideIndicators.contains(new Point(point.row - 1, point.column))) {
+                            sides++;
+                        }
+                        sideIndicators.add(point);
+                    }
+                }
+            }
+
+            //right
+            for (Integer key : columns.keySet()) {
+                List<Point> column = columns.get(key);
+                Set<Point> sideIndicators = new HashSet<>();
+                for (Point point : column) {
+                    if (!points.contains(new Point(point.row, point.column + 1))) {
+                        if (!sideIndicators.contains(new Point(point.row - 1, point.column))) {
+                            sides++;
+                        }
+                        sideIndicators.add(point);
+                    }
+                }
+            }
+            return sides;
+        }
+    }
+
+    private static class Point implements Comparable<Point> {
+        int row, column;
+
+        Point(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        @Override
+        public int compareTo(Point p) {
+            return (this.column != p.column) ? this.column - p.column : this.row - p.row;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + row + ", " + column + ")";
+        }
+
+        @Override
+        public int hashCode() {
+            int prime = 31;
+            int result = 1;
+            result = prime * result + row;
+            result = prime * result + column;
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true; // Check if the same reference
+            if (obj == null || getClass() != obj.getClass()) return false; // Null or different class
+            Point other = (Point) obj; // Cast to Point
+            return this.column == other.column && this.row == other.row; // Compare fields
         }
     }
 }
