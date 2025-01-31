a658e43a1f62ee1e011c863abe9b104b6ca0c547
tvece <t.vecera97@gmail.com>
2025-01-31 18:58:32 +0100

    D05 cleanup + split (2023)

--- src/main/java/com/advent/_2023/D05.java
+++ src/main/java/com/advent/_2023/D05.java
@@ -8,111 +8,108 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class D05 {
+public class D05P2 {
 
-	//TODO: optimize performance
-	public static void main(String[] args) {
-		Path filePath = Paths.get("src/main/resources/2023/D05.txt");
-		List<String> lines = new ArrayList<String>();
-		try {
-			lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
-		} catch (IOException e) {
-			throw new RuntimeException("Failed to read input data!", e);
-		}
+    //TODO: optimize performance
+    public static void main(String[] args) {
+        Path filePath = Paths.get("src/main/resources/2023/D05.txt");
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read input data!", e);
+        }
 
-		List<List<Transformation>> transfomations = new ArrayList<List<Transformation>>();
-		int index = 3;
-		List<Transformation> ss = new ArrayList<>();
-		index = fillTransformations(ss, index, lines);
-		ss.sort((a, b) -> (int) (a.source - b.source));
-		transfomations.add(ss);
-		List<Transformation> sf = new ArrayList<>();
-		index = fillTransformations(sf, index + 2, lines);
-		sf.sort((a, b) -> (int) (a.source - b.source));
-		transfomations.add(sf);
-		List<Transformation> fw = new ArrayList<>();
-		index = fillTransformations(fw, index + 2, lines);
-		fw.sort((a, b) -> (int) (a.source - b.source));
-		transfomations.add(fw);
-		List<Transformation> wl = new ArrayList<>();
-		index = fillTransformations(wl, index + 2, lines);
-		wl.sort((a, b) -> (int) (a.source - b.source));
-		transfomations.add(wl);
-		List<Transformation> lt = new ArrayList<>();
-		index = fillTransformations(lt, index + 2, lines);
-		lt.sort((a, b) -> (int) (a.source - b.source));
-		transfomations.add(lt);
-		List<Transformation> th = new ArrayList<>();
-		index = fillTransformations(th, index + 2, lines);
-		th.sort((a, b) -> (int) (a.source - b.source));
-		transfomations.add(th);
-		List<Transformation> hl = new ArrayList<>();
-		index = fillTransformations(hl, index + 2, lines);
-		hl.sort((a, b) -> (int) (a.source - b.source));
-		transfomations.add(hl);
+        List<List<Transformation>> transformations = new ArrayList<>();
+        int index = 3;
+        List<Transformation> ss = new ArrayList<>();
+        index = fillTransformations(ss, index, lines);
+        ss.sort((a, b) -> (int) (a.source - b.source));
+        transformations.add(ss);
+        List<Transformation> sf = new ArrayList<>();
+        index = fillTransformations(sf, index + 2, lines);
+        sf.sort((a, b) -> (int) (a.source - b.source));
+        transformations.add(sf);
+        List<Transformation> fw = new ArrayList<>();
+        index = fillTransformations(fw, index + 2, lines);
+        fw.sort((a, b) -> (int) (a.source - b.source));
+        transformations.add(fw);
+        List<Transformation> wl = new ArrayList<>();
+        index = fillTransformations(wl, index + 2, lines);
+        wl.sort((a, b) -> (int) (a.source - b.source));
+        transformations.add(wl);
+        List<Transformation> lt = new ArrayList<>();
+        index = fillTransformations(lt, index + 2, lines);
+        lt.sort((a, b) -> (int) (a.source - b.source));
+        transformations.add(lt);
+        List<Transformation> th = new ArrayList<>();
+        index = fillTransformations(th, index + 2, lines);
+        th.sort((a, b) -> (int) (a.source - b.source));
+        transformations.add(th);
+        List<Transformation> hl = new ArrayList<>();
+        fillTransformations(hl, index + 2, lines);
+        hl.sort((a, b) -> (int) (a.source - b.source));
+        transformations.add(hl);
 
-		String firstLine = lines.get(0);
-		firstLine = firstLine.substring(firstLine.indexOf(" ") + 1);
-		List<Double> seedNumbers = Arrays.asList(firstLine.split(" ")).stream()
-				.map(string -> Double.parseDouble(string)).collect(Collectors.toList());
+        String firstLine = lines.getFirst();
+        firstLine = firstLine.substring(firstLine.indexOf(" ") + 1);
+        List<Double> seedNumbers = Arrays.stream(firstLine.split(" "))
+                .map(Double::parseDouble).toList();
 
-		Double minimum = Double.MAX_VALUE;
-		for (int i = 0; i < seedNumbers.size(); i += 2) {
-			Double start = seedNumbers.get(i);
-			Double range = seedNumbers.get(i + 1);
-			for (int j = 0; j < range; j++) {
-				Double transfomedValue = start + j;
-				for (List<Transformation> transfomer : transfomations) {
-					transfomedValue = transform(transfomedValue, transfomer);
-				}
-				if (minimum > transfomedValue) {
-					minimum = transfomedValue;
-				}
-			}
-		}
+        Double minimum = Double.MAX_VALUE;
+        for (int i = 0; i < seedNumbers.size(); i += 2) {
+            Double start = seedNumbers.get(i);
+            Double range = seedNumbers.get(i + 1);
+            for (int j = 0; j < range; j++) {
+                Double transfomedValue = start + j;
+                for (List<Transformation> transformer : transformations) {
+                    transfomedValue = transform(transfomedValue, transformer);
+                }
+                if (minimum > transfomedValue) {
+                    minimum = transfomedValue;
+                    System.out.printf("%.9f", minimum);
+                }
+            }
+        }
+        System.out.printf("%.9f", minimum);
+    }
 
-		System.out.printf("%.9f", minimum);
+    private static int fillTransformations(List<Transformation> transformations, int index, List<String> lines) {
+        String line = lines.get(index);
+        while (!line.isEmpty()) {
+            List<Double> record = Arrays.stream(line.split(" ")).map(Double::parseDouble).toList();
+            transformations.add(new Transformation(record.get(1), record.get(0), record.get(2)));
+            index++;
+            if (index == lines.size()) {
+                return index;
+            }
+            line = lines.get(index);
+        }
+        return index;
+    }
 
-	}
+    private static Double transform(Double source, List<Transformation> transformations) {
+        for (Transformation transformation : transformations) {
+            if (source < transformation.source) {
+                return source;
+            }
+            if (source < (transformation.source + transformation.range)) {
+                return source - transformation.source + transformation.destination;
+            }
+        }
+        return source;
+    }
 
-	private static int fillTransformations(List<Transformation> transformations, int index, List<String> lines) {
-		String line = lines.get(index);
-		while (!line.isEmpty()) {
-			List<Double> record = Arrays.asList(line.split(" ")).stream().map(string -> Double.parseDouble(string))
-					.collect(Collectors.toList());
-			transformations.add(new Transformation(record.get(1), record.get(0), record.get(2)));
-			index++;
-			if (index == lines.size()) {
-				return index;
-			}
-			line = lines.get(index);
-		}
-		return index;
-	}
+    private static class Transformation {
+        public Double source;
+        public Double destination;
+        public Double range;
 
-	private static Double transform(Double source, List<Transformation> transformations) {
-		for (Transformation transformation : transformations) {
-			if (source < transformation.source) {
-				return source;
-			}
-			if (source < (transformation.source + transformation.range)) {
-				return source - transformation.source + transformation.destination;
-			}
-		}
-		return source;
-	}
-
-	private static class Transformation {
-		public Double source;
-		public Double destination;
-		public Double range;
-
-		public Transformation(Double source, Double destination, Double range) {
-			this.source = source;
-			this.destination = destination;
-			this.range = range;
-		}
-	}
+        public Transformation(Double source, Double destination, Double range) {
+            this.source = source;
+            this.destination = destination;
+            this.range = range;
+        }
+    }
 }
