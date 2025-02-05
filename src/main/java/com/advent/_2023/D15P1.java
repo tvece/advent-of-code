44e71663127c6a637066b3e97177b976cbad70ef
tvece <t.vecera97@gmail.com>
2025-02-05 23:04:58 +0100

    D15 cleanup + split (2023)

---
+++ src/main/java/com/advent/_2023/D15P1.java
@@ -0,0 +1,37 @@
+package com.advent._2023;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class D15P1 {
+
+    public static void main(String[] args) {
+        Path filePath = Paths.get("src/main/resources/2023/D15.txt");
+        String row;
+        try {
+            row = Files.readAllLines(filePath, StandardCharsets.UTF_8).getFirst();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read input data!", e);
+        }
+
+        int result = 0;
+        for (String step : row.split(",")) {
+            result += getHash(step);
+        }
+
+        System.out.println(result);
+    }
+
+    private static int getHash(String step) {
+        int stepResult = 0;
+        for (char character : step.toCharArray()) {
+            stepResult += character;
+            stepResult = stepResult * 17;
+            stepResult = stepResult % 256;
+        }
+        return stepResult;
+    }
+}
---
+++ src/main/java/com/advent/_2023/D15P2.java
@@ -0,0 +1,66 @@
+package com.advent._2023;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class D15P2 {
+
+    public static void main(String[] args) {
+        Path filePath = Paths.get("src/main/resources/2023/D15.txt");
+        String row;
+        try {
+            row = Files.readAllLines(filePath, StandardCharsets.UTF_8).getFirst();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read input data!", e);
+        }
+
+        List<List<String>> boxes = new ArrayList<>();
+        for (int i = 0; i < 256; i++) {
+            boxes.add(new ArrayList<>());
+        }
+
+        String[] steps = row.split(",");
+        for (String step : steps) {
+            if (step.contains("-")) {
+                String lensId = step.substring(0, step.length() - 1);
+                List<String> box = boxes.get(getHash(lensId));
+                box.stream().filter(lens -> lens.startsWith(lensId)).findFirst().ifPresent(box::remove);
+            } else {
+                String lensId = step.substring(0, step.indexOf("="));
+                List<String> box = boxes.get(getHash(lensId));
+                String existingLens = box.stream().filter(lens -> lens.startsWith(lensId)).findFirst().orElse(null);
+                if (existingLens == null) {
+                    box.add(step);
+                } else {
+                    box.set(box.indexOf(existingLens), step);
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < 256; i++) {
+            List<String> box = boxes.get(i);
+            for (int j = 0; j < box.size(); j++) {
+                String lens = box.get(j);
+                int focalLength = Integer.parseInt(lens.substring(lens.indexOf("=") + 1));
+                result += (i + 1) * (j + 1) * focalLength;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static int getHash(String step) {
+        int stepResult = 0;
+        for (char character : step.toCharArray()) {
+            stepResult += character;
+            stepResult = stepResult * 17;
+            stepResult = stepResult % 256;
+        }
+        return stepResult;
+    }
+}
